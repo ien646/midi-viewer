edf58df16fa1dd119f01e68d0d932c05d4508b6f
ien <ien@amsostudios.com>
2026-01-22 00:01:13 +0100

    make everything configurable from config.cfg

--- background.gd
+++ background.gd
@@ -1,21 +1,10 @@
 extends Sprite3D
 
 func _ready() -> void:
-	for arg in OS.get_cmdline_args():
-		if(arg.begins_with("--bgimagedarken")):
-			var darken_value = float(arg.split("=")[1]);
-			modulate = Color(1.0 - darken_value, 1.0 - darken_value, 1.0 - darken_value);
-		elif(arg.begins_with("--bgimage")):
-			var image_path_segments = arg.split("=");
-			image_path_segments.remove_at(0);	
-			var image_path = "=".join(image_path_segments);
-			print("Using custom background image: ", image_path);
-			
-			if(image_path.begins_with("\"")):
-				image_path = image_path.substr(1);
-			if(image_path.ends_with("\"")):
-				image_path = image_path.substr(0, image_path.length() - 1);
-			
-			var image = Image.load_from_file(image_path);
-			
-			self.texture = ImageTexture.create_from_image(image);	
+	var bgimage = Config.get_value("Background", "image") as String;
+	var bgimage_darken = Config.get_value("Background", "image-darken") as float;
+	
+	if(FileAccess.file_exists(bgimage)):
+		var image = Image.load_from_file(bgimage);
+		texture = ImageTexture.create_from_image(image);
+		modulate = Color(1.0 - bgimage_darken, 1.0 - bgimage_darken, 1.0 - bgimage_darken);
--- grid.gd
+++ grid.gd
@@ -1,8 +1,10 @@
 extends MultiMeshInstance3D
 
+# Near / Middle / Far
+@export var config_tag = "Near";
+
 @export_group("Generation")
 @export var block_mesh: Mesh;
-@export var gradient: Gradient;
 @export var row_size = 12
 @export var col_separation = 0.2;
 @export var row_separation = 0.5;
@@ -17,6 +19,7 @@ extends MultiMeshInstance3D
 
 @export_group("Appearance")
 @export var material: Material = StandardMaterial3D.new();
+@export var gradient: Gradient;
 @export var color_gravity = 0.2;
 @export var min_color_value = 0.02;
 @export var gradient_hue_shift_speed = 0.2;
@@ -36,60 +39,101 @@ extends MultiMeshInstance3D
 @export var midi_last_note = 108;
 @export var midi_port = 16;
 
-var positions: Array[Vector3]
-var rotations: Array[Vector3]
-var scales: Array[Vector3]
-var colors: Array[Color]
+const CONFIG_FILE = "config.cfg";
+
+var positions: Array[Vector3] = [];
+var rotations: Array[Vector3] = [];
+var scales: Array[Vector3] = [];
+var colors: Array[Color] = [];
 var hue_shift = 0;
 
 var note_queue_mutex = Mutex.new();
-var note_queue = [];
+var note_queue: Array[Vector2i] = [];
 
 var note_count = midi_last_note - midi_first_note + 1;
 
-var mux = Mutex.new()
-
-var midi_poll_process_stdio: FileAccess;
-var midi_poll_process_pid: int;
-var midi_poll_thread: Thread;
+var linux_midi_poll_process_stdio: FileAccess;
+var linux_midi_poll_process_pid: int;
+var linux_midi_poll_thread: Thread;
 
 var particles_pool: Array[GPUParticles3D];
 var particles_pool_index = 0;
 
 var quit_requested = false;
 
-func _poll_midi_events_linux():
+func get_config_value(section: String, key: String) -> Variant:
+	return Config.get_tagged_value(section, key, config_tag);
+	
+func init_config_values():
+	# -- Block Gradient --
+	var gradient_offsets = get_config_value("Gradient", "offsets") as Array[float];
+	var gradient_colors = get_config_value("Gradient", "colors") as Array[Color];
+	var gradient_interp = get_config_value("Gradient", "interpolate") as bool;
+	var imported_gradient = Gradient.new();
+	imported_gradient.interpolation_mode = Gradient.GRADIENT_INTERPOLATE_LINEAR if gradient_interp else Gradient.GRADIENT_INTERPOLATE_CONSTANT;
+	imported_gradient.offsets = gradient_offsets;
+	imported_gradient.colors = gradient_colors;
+	self.gradient = imported_gradient;
+	
+	# -- Block Generation --
+	position = get_config_value("Block-Generation", "position") as Vector3;
+	col_separation = get_config_value("Block-Generation", "column-separation") as float;
+	row_separation = get_config_value("Block-Generation", "row-separation") as float;
+	row_size = get_config_value("Block-Generation", "notes-per-row") as int;
+	min_y_scale =  get_config_value("Block-Generation", "min-y-scale") as float;
+	max_y_scale = get_config_value("Block-Generation", "max-y-scale") as float;
+	
+	# -- Block Behaviour --
+	gravity = get_config_value("Block-Behaviour", "gravity") as float;
+	rotation_speed = get_config_value("Block-Behaviour", "rotation-speed") as float;
+	
+	# -- Block Appearance --
+	color_gravity = get_config_value("Block-Appearance", "color-gravity") as float;
+	min_color_value = get_config_value("Block-Appearance", "min-color-value") as float;
+	gradient_hue_shift_speed = get_config_value("Block-Appearance", "gradient-hue-shift-speed") as float;
+	
+	# -- Particles --
+	particles_disabled = !get_config_value("Particles", "enabled") as bool;
+	min_particles_hit = get_config_value("Particles", "min") as int;
+	max_particles_hit = get_config_value("Particles", "max") as int;
+	particle_count_curve = get_config_value("Particles", "velocity-curve-pow") as float;
+	max_particle_systems = get_config_value("Particles", "pool-size") as int;
+
+func linux_enqueue_note(note: Vector2i):
+	note_queue_mutex.lock();
+	note_queue.push_back(note);
+	note_queue_mutex.unlock();
+	
+func linux_aseqdump_line_to_note(line: String) -> Vector2i:
+	line = line.split(" 0, ")[1];
+	line = line.replace("note", "").replace("velocity", "");
+	var segments = line.split(',');
+	return Vector2i(int(segments[0]), int(segments[1]));
+	
+func linux_midi_poll_thread_routine():
+	while(!quit_requested):
+		var line = linux_midi_poll_process_stdio.get_line();
+		if (line.contains("Note on")):
+			linux_enqueue_note(linux_aseqdump_line_to_note(line));
+
+func linux_poll_midi_events():
 	# aseqdump will write a line with every midi event received
 	var midi_process = OS.execute_with_pipe("aseqdump", ["-p", str(midi_port)]);
-	midi_poll_process_stdio = midi_process["stdio"];
-	midi_poll_process_pid = midi_process["pid"];
+	linux_midi_poll_process_stdio = midi_process["stdio"];
+	linux_midi_poll_process_pid = midi_process["pid"];
 	
-	midi_poll_thread = Thread.new();
-	midi_poll_thread.start(func():
-		while(!quit_requested):
-			var line = midi_poll_process_stdio.get_line();
-			if (line.contains("Note on")):
-				line = line.split(" 0, ")[1];
-				line = line.replace("note", "").replace("velocity", "");
-				var segments = line.split(',');
-				var note = int(segments[0]);
-				var velocity = int(segments[1]);
-				
-				note_queue_mutex.lock();
-				note_queue.push_back(note);
-				note_queue.push_back(velocity);
-				note_queue_mutex.unlock();
-	)
+	linux_midi_poll_thread = Thread.new();
+	linux_midi_poll_thread.start(linux_midi_poll_thread_routine);
 
 func poll_midi_events() -> void:	
 	if (OS.get_name() == "Linux"):
-		_poll_midi_events_linux();
+		linux_poll_midi_events();
 		
 	elif (OS.get_name() == "Windows"):
 		OS.open_midi_inputs();
-
+		
 func _ready() -> void:
-	_process_commandline_args();
+	init_config_values();
 	
 	multimesh = MultiMesh.new();
 	multimesh.transform_format = MultiMesh.TRANSFORM_3D;
@@ -110,9 +154,13 @@ func _ready() -> void:
 	
 	for i in range(0, note_count):
 		var x = i % row_size;
+		
+		@warning_ignore("integer_division")
 		var y = i / row_size;
 		
+		@warning_ignore("integer_division")
 		var x_pos = (x - (row_size / 2)) * (1 + col_separation);
+		@warning_ignore("integer_division")
 		var y_pos = (y - (col_size / 2)) * (1 + row_separation);
 		
 		positions.push_back(Vector3(x_pos, 0, y_pos));
@@ -121,10 +169,6 @@ func _ready() -> void:
 		colors.push_back(Color.BLACK);
 	
 	poll_midi_events();
-	
-	var cam_node: Camera3D = get_parent().get_node("Camera3D") as Camera3D;
-	cam_node.environment.background_mode= Environment.BG_COLOR;
-	cam_node.environment.background_color = background_color;
 	
 func _process(delta: float) -> void:	
 	for i in range(0, note_count):
@@ -143,14 +187,11 @@ func _process(delta: float) -> void:
 	hue_shift = fmod(hue_shift, 1);
 	
 	note_queue_mutex.lock();
-	for i in range(0, note_queue.size() - 1):
-		var note = note_queue[i];
-		var velocity = note_queue[i+1];
-		_process_midi_event(note, velocity);		
-		i += 1;		
+	for n in note_queue:
+		_process_midi_event(n[0], n[1]);
 	note_queue.clear();
-	note_queue_mutex.unlock();	
-	
+	note_queue_mutex.unlock();
+
 func _process_midi_event(note: int, vel: int) -> void:
 	var i = note - midi_first_note;
 	var v = vel * midi_velocity_scale;
@@ -160,7 +201,7 @@ func _process_midi_event(note: int, vel: int) -> void:
 	scales[i].y = y_position;
 	var color_sample_point = remap(i, 0, note_count, 0, 1) + hue_shift;
 	colors[i] = gradient.sample(fmod(color_sample_point, 1));
-	
+
 	if (!particles_disabled):
 		var particles = particles_pool[particles_pool_index];
 		particles.emitting = false;
@@ -175,13 +216,12 @@ func _process_midi_event(note: int, vel: int) -> void:
 		particles_pool_index = particles_pool_index % max_particle_systems;
 		particles.position = positions[i] + Vector3(0, y_position*2, 0);
 		particles.emitting = true;
-		
-		
+
 # Windows only
 func _input(event: InputEvent) -> void:
 	if (event is InputEventMIDI):
 		_process_midi_event(event.pitch, event.velocity);
-		
+
 func _print_midi_info(midi_event: InputEventMIDI):
 	if midi_event.message == MIDIMessage.MIDI_MESSAGE_NOTE_ON || midi_event.message == MIDIMessage.MIDI_MESSAGE_NOTE_OFF:
 		print("Pitch:", midi_event.pitch, " | Velocity:", midi_event.velocity);
@@ -191,20 +231,8 @@ func _notification(what):
 		quit_requested = true;
 		
 		if(OS.get_name() == "Linux"):
-			midi_poll_thread.wait_to_finish();
-			OS.kill(midi_poll_process_pid);
+			linux_midi_poll_thread.wait_to_finish();
+			OS.kill(linux_midi_poll_process_pid);
 			OS.execute("killall", ["aseqdump"]);
 		
 		get_tree().quit();
-		
-func _process_commandline_args():
-	for arg in OS.get_cmdline_args():
-		if(arg.begins_with("--bgcolor")):
-			var values_rgb = arg.split("=")[1].split(',');
-			background_color = Color(float(values_rgb[0]), float(values_rgb[1]), float(values_rgb[2]));
-			print("Using user provided background color: [", background_color, "]");
-		if(arg == "--noparticles"):
-			particles_disabled = true
-		if(arg.begins_with("--midiport")):
-			var port = int(arg.split("=")[1]);
-			midi_port = port;
