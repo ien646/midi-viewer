34484cdae7f5cb9b84c52d12e74f14b2bbf1412e
ien <ien@amsostudios.com>
2026-01-22 05:27:02 +0100

    fix fullscreen toggle

--- controls.gd
+++ controls.gd
@@ -4,7 +4,7 @@ var fullscreen = false;
 
 func _input(event: InputEvent) -> void:
 	if(event is InputEventKey):
-		if(event.as_text_keycode() == "F"):
+		if(event.is_pressed() && event.as_text_keycode() == "F"):
 			if (!fullscreen):
 				DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN);
 			else:
