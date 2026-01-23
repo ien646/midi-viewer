30d27b0f29f374a495ff030588229b51ee0f44f7
ien <ien@amsostudios.com>
2026-01-23 20:22:35 +0100

    update aa settings from settings menu

--- scripts/subviewport.gd
+++ scripts/subviewport.gd
@@ -1,6 +1,12 @@
 extends SubViewport
 
-func _ready():
+@export var config_node: Node;
+
+func handle_config_value_changed(section: String, key: String, _value: Variant):
+	if(section == "Graphics" && (key == "FXAA" || key == "SMAA")):
+		update_aa();
+
+func update_aa():
 	var fxaa: bool = Config.get_value("Graphics", "FXAA");
 	var smaa: bool = Config.get_value("Graphics", "SMAA");
 	
@@ -10,6 +16,10 @@ func _ready():
 		screen_space_aa = Viewport.SCREEN_SPACE_AA_FXAA;
 	else:
 		screen_space_aa = Viewport.SCREEN_SPACE_AA_DISABLED
+
+func _ready():
+	config_node.connect("config_value_changed", handle_config_value_changed);
+	update_aa();
 
 func _process(_delta: float) -> void:
 	if(size != DisplayServer.window_get_size()):
