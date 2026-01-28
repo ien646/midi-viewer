31ba5bb4264912b1e495d69326158dacdb234d7e
ien <ien@amsostudios.com>
2026-01-28 11:06:34 +0100

    Revamp settings menu

--- scripts/hint.gd
+++ scripts/hint.gd
@@ -4,8 +4,15 @@ extends Label
 
 var lifetime = 0;
 
+func _ready():
+	visible = true;
+
 func _process(delta: float) -> void:
 	lifetime += delta;
 	if(lifetime >= lifetime_seconds):
 		queue_free();
+		
+func _input(event: InputEvent):
+	if(event is InputEventKey && (event as InputEventKey).keycode == KEY_ESCAPE):
+		lifetime += lifetime_seconds;
 	
